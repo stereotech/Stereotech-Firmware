2e9c1e06a9d55447fe8910ae1a2b8c6f4da6637c
Ilya Gushchin <frylock3434@gmail.com>
2020-08-04 14:28:34 +0400

    no message

--- src/modules/tools/zprobe/FiveAxisStrategy.cpp
+++ src/modules/tools/zprobe/FiveAxisStrategy.cpp
@@ -101,9 +101,10 @@ bool FiveAxisStrategy::handleConfig()
     //if (!p10.empty())
     //    probe_points[9] = parseXYZ(p10.c_str());
 
-    big_part_length = THEKERNEL->config->value(leveling_strategy_checksum, five_axis_strategy_checksum, probe_device_big_part_length)->by_default(10.0F)->as_number();
-    small_part_length = THEKERNEL->config->value(leveling_strategy_checksum, five_axis_strategy_checksum, probe_device_small_part_length)->by_default(10.0F)->as_number();
-    home = THEKERNEL->config->value(leveling_strategy_checksum, five_axis_strategy_checksum, home_checksum)->by_default(true)->as_bool();
+    float big_part_length = THEKERNEL->config->value(leveling_strategy_checksum, five_axis_strategy_checksum, probe_device_big_part_length)->by_default(10.0F)->as_number();
+    float small_part_length = THEKERNEL->config->value(leveling_strategy_checksum, five_axis_strategy_checksum, probe_device_small_part_length)->by_default(10.0F)->as_number();
+    bool home = THEKERNEL->config->value(leveling_strategy_checksum, five_axis_strategy_checksum, home_checksum)->by_default(true)->as_bool();
+    this->home = home;
 
     for (int i = 0; i < 9; i++)
     {
